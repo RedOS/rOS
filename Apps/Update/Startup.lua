f691f0966c67fc4910594e95c8178c73d32e8330
Red OS Team <milous.mrtvej@gmail.com>
2015-04-25 17:50:31 +0200

    Fix

--- Apps/Update/Startup.lua
+++ Apps/Update/Startup.lua
@@ -1,72 +1,29 @@
-local args = {"RedOS","rOS"}
-term.setTextColor(1)
-Data=Core.getData()
-if http.get("https://raw.githubusercontent.com/RedOS/rOS/master/System/Version.lua").readAll()==Data.Version and not force then
-paintutils.drawFilledBox(w/2-9,h/2-1,w/2+9,h/2+2,128)
-term.setCursorPos(w/2-7,h/2)
-term.setTextColor(1)
-term.setBackgroundColor(128)
-print("No update found")
-term.setCursorPos(w/2-6,h/2+1)
-print("Force update?")
-term.setCursorPos(w/2-7,h/2+2)
-print("Yes")
-term.setCursorPos(w/2+6,h/2+2)
-print("No")
-local update=true
-while update do
-local tEvent={os.pullEventRaw()}
-if tEvent[1]=="mouse_click" then
-if tEvent[4]>=h/2-1 and tEvent[4]<=h/2+2 then
-if tEvent[4]==h/2+2 then
-if tEvent[3]>=w/2-8 and tEvent[3]<=w/2-4 then
-force=true
-update=false
-shell.run("Apps/Update/Startup.lua")
+local size=0
+local function getSize(number,round)
+if number<1024 then
+unit=" B"
 else
-update=false
-shell.run("System/Desktop.lua")
-end
-end
-else
-update=false
-shell.run("System/Desktop.lua")
-end
-elseif tEvent[1]=="key" then
-if tEvent[2]==keys.y then
-force=true
-update=false
-shell.run("Apps/Update/Startup.lua")
-else
-update=false
-shell.run("System/Desktop.lua")
+local units={" kB"," MB"," GB"," TB"," PB"}
+for i=1,5 do
+if number/1024^i < 768 then
+number=number/1024^i
+unit=units[i]
+break
 end
 end
 end
-else
-Draw.setStatusColor(128)
+number=tostring(number)
+if number:find(".",1,true) then number=number:sub(1,number:find(".",1,true)+2) end
+number=tonumber(number)
+return number, unit
+end
+Draw.setStatusColor(1)
 Draw.isStatusVisible(true)
 Draw.status()
-local function loading()
-local img=paintutils.loadImage("Apps/Update/load")
-paintutils.drawFilledBox(w/2-7,h/2-1,w/2+7,h/2+2,128)
-paintutils.drawImage(img,w/2-4,h/2+1)
-term.setCursorPos((w-9)/2,h/2)
-term.setTextColor(1)
-term.setBackgroundColor(128)
-print("Updating...")
-for i=1,5 do
-paintutils.drawImage(img,w/2-4,h/2+1)
-paintutils.drawPixel(w/2-4+2*(i-1),h/2+1,1)
-os.sleep(.085)
-end
-paintutils.drawImage(img,w/2-4,h/2+1)
-end
-args[3] = args[3] or "master"
-args[4] = args[4] or ""
-if not automated and #args < 2 then
-        print("Usage:\n"..shell.getRunningProgram().." <user> <repo> [branch=master] [path=/]") error()
-end
+Draw.clear(1)
+if http then local tUpdates=http.get("github.com/url") end
+if tUpdates then
+tUpdates=textutils.unserialize(tUpdates.readAll())
 local function save(data,file)
         local file = shell.resolve(file)
         if not (fs.exists(string.sub(file,1,#file - #fs.getName(file))) and fs.isDir(string.sub(file,1,#file - #fs.getName(file)))) then
@@ -81,30 +38,26 @@ local function download(url, file)
         save(http.get(url).readAll(),file)
 end
 if not fs.exists("Apps/Update/json") then
-        print("")
         download("http://pastebin.com/raw.php?i=4nRg9CHU","Apps/Update/json")
 end
 os.loadAPI("Apps/Update/json")
-loading()
-if pre_dl then loadstring(pre_dl)() else print("") end
-local data = json.decode(http.get("https://api.github.com/repos/"..args[1].."/"..args[2].."/git/trees/"..args[3].."?recursive=1").readAll())
+if pre_dl then loadstring(pre_dl)() end
+local data = json.decode(http.get("https://api.github.com/repos/RedOS/rOS/git/trees/master?recursive=1").readAll())
 if data.message and data.message == "Not found" then error("Invalid repository",2) else
 	for k,v in pairs(data.tree) do
-	loading()
-		-- Make directories
+		if v.size then
+			size=size+v.size
+		end
+	end
+	
+	for k,v in pairs(data.tree) do
 		if v.type == "tree" then
-			fs.makeDir(fs.combine(args[4],v.path))
-			if not hide_progress then
-			end
+			fs.makeDir(v.path)
 		end
 	end
 	for k,v in pairs(data.tree) do
-	loading()
-		-- Download files
 		if v.type == "blob" then
-			download("https://raw.github.com/"..args[1].."/"..args[2].."/"..args[3].."/"..v.path,fs.combine(args[4],v.path))
-			if not hide_progress then
-			end
+			download("https://raw.github.com/RedOS/rOS/master/"..v.path,v.path)
 		end
 	end
 end
@@ -122,5 +75,4 @@ end
 os.sleep(0.01)
 os.reboot()
 end
-update=false
 shell.run("System/Desktop.lua")
